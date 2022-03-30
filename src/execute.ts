340bab882828bf64dfa2b1b29cf93c294b95af4c
Nils Söderman <rymdnisse@live.com>
2022-03-30 21:58:01 +0200

    Added option to execute batch file in cmd + added some comments/docstrings

--- src/execute.ts
+++ src/execute.ts
@@ -1,19 +1,35 @@
 import * as vscode from 'vscode';
 
 import * as path from 'path';
-// import * as child_process from 'child_process';
+import * as child_process from 'child_process';
 
 
 const TERMINAL_NAME = "Batch Runner";
 
 
+/**
+ * @returns The workspace configuration for this extension _('batchrunner')_
+ */
+export function getExtensionConfig() {
+    return vscode.workspace.getConfiguration("batchrunner");
+}
+
+
+/**
+ * @param filepath The filepath to the batch file
+ * @returns true if the filepath is a batch file, otherwise false
+ */
 function isBatchFile(filepath: string) {
     return filepath.toLowerCase().endsWith(".bat");
 }
 
 
-function getTerminal(bEnsureExists = true)
-{
+/**
+ * Get the 'Batch Runner' terminal used by this extension
+ * @param bEnsureExists If terminal doesn't exist, create it
+ * @returns The terminal if one could be found/created
+ */
+function getBatchRunnerTerminal(bEnsureExists = true) {
     for (const terminal of vscode.window.terminals) {
         if (terminal.name === TERMINAL_NAME) {
             return terminal;
@@ -25,25 +41,55 @@ function getTerminal(bEnsureExists = true)
     }
 }
 
+
+/**
+ * Run a batch file in the VSCode terminal
+ * @param filepath Absolute filepath to the .bat file
+ * @returns true if the batch file was executed
+ */
 function runBatchFileInTerminal(filepath: string) {
-    const terminal = getTerminal();
+    const terminal = getBatchRunnerTerminal();
     if (!terminal) {
         return false;
     }
+
     terminal.sendText(`cmd /c "${filepath}"`, true);
     terminal.show();
 
+    return true;
 }
 
+/**
+ * Open a batch file in cmd
+ * @param filepath Absolute filepath to the batch file
+ */
+function runBatchFileInCmd(filepath: string) {
+    const command = `start cmd /c "${filepath}"`;
+    child_process.exec(command);
+}
+
+
+/**
+ * Main function to be called from the extension.ts
+ * This will run the batch file taking into consideration the user's settings etc.
+ * @param filepath The absolute filepath to the batch file
+ * @returns true if the batch file could be exectued, otherwise false
+ */
 export function runBatchFile(filepath: string) {
+    // Make sure the filepath is a batch file
     if (!isBatchFile(filepath)) {
         const filename = path.basename(filepath);
         vscode.window.showErrorMessage(`Batch Runner: ${filename} was not recognized as a batch file.`);
         return false;
     }
-    
-    // const a = child_process.exec(`cmd /c start ${filepath}`);
-    runBatchFileInTerminal(filepath);
-    
-    return true;
+
+    // Check where we should run the batch file
+    const config = getExtensionConfig();
+    const runBatchIn: string | undefined = config.get("runBatchIn");
+    if (runBatchIn?.toLowerCase() === "cmd") {
+        return runBatchFileInCmd(filepath);
+    }
+    else {
+        return runBatchFileInTerminal(filepath);
+    }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -9,12 +9,14 @@ export function activate(context: vscode.ExtensionContext) {
 			let filepath: string | undefined;
 
 			if (data) {
+				// Filepath provided by as an argument (when e.g. running it from the context menu)
 				filepath = data["path"];
 				if (filepath && filepath.startsWith("/")) {
 					filepath = filepath.replace("/", "");
 				}
 			}
 			else {
+				// No filepath provided as an argument, get the currently active file instead (e.g. when using the Hotkey)
 				filepath = vscode.window.activeTextEditor?.document.uri.fsPath;
 			}
 
