68cba1123592706e98877419251d835ac955fa70
Nils Söderman <rymdnisse@live.com>
2022-08-14 14:46:31 +0200

    Added command `batch-utils.execBatchFileAsAdmin`

--- src/extension.ts
+++ src/extension.ts
@@ -5,26 +5,40 @@ import * as execute from './execute';
 export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(
-		vscode.commands.registerCommand('batch-utils.execBatchFile', (data) => {
-			let filepath: string | undefined;
-
-			if (data) {
-				// Filepath provided by as an argument (when e.g. running it from the context menu)
-				filepath = data["path"];
-				if (filepath && filepath.startsWith("/")) {
-					filepath = filepath.replace("/", "");
-				}
-			}
-			else {
-				// No filepath provided as an argument, get the currently active file instead (e.g. when using the Hotkey)
-				filepath = vscode.window.activeTextEditor?.document.uri.fsPath;
-			}
+		vscode.commands.registerCommand('batch-utils.execBatchFile', (args) => {
+			let filepath = getFilepath(args);
 
 			if (filepath) {
-				execute.runBatchFile(filepath);
+				execute.runBatchFile(filepath, false);
+			}
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('batch-utils.execBatchFileAsAdmin', (args) => {
+			let filepath = getFilepath(args);
+			
+			if (filepath) {
+				execute.runBatchFile(filepath, true);
 			}
 		})
 	);
 }
 
+
 export function deactivate() { }
+
+
+function getFilepath(args: any) {
+	if (args) {
+		// Filepath provided by as an argument (when e.g. running it from the context menu)
+		let filepath: string = args["path"];
+		if (filepath && filepath.startsWith("/")) {
+			filepath = filepath.replace("/", "");
+		}
+		return filepath;
+	}
+	
+	// No filepath provided as an argument, get the currently active file instead (e.g. when using the Hotkey)
+	return vscode.window.activeTextEditor?.document.uri.fsPath;
+}
